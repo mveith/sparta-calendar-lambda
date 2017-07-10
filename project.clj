a48d1b007e747acbc3b35e74d0e1f4606eab4512
mveith <mveith@hotmail.cz>
2017-07-10 22:08:33 +0200

    Download html

--- project.clj
+++ project.clj
@@ -4,11 +4,13 @@
   :dependencies [[org.clojure/clojure       "1.8.0"]
                  [org.clojure/clojurescript "1.8.51"]
                  [org.clojure/core.async    "0.2.395"]
-                 [io.nervous/cljs-lambda    "0.3.5"]]
+                 [io.nervous/cljs-lambda    "0.3.5"]
+                 [io.nervous/eulalie "0.6.10"]]
   :plugins [[lein-cljsbuild "1.1.4"]
             [lein-npm       "0.6.0"]
             [io.nervous/lein-cljs-lambda "0.6.6"]]
-  :npm {:dependencies [[source-map-support "0.4.0"]]}
+  :npm {:dependencies [
+    [source-map-support "0.4.0"]]}
   :source-paths ["src"]
   :cljs-lambda
   {:defaults      {:role ""}
--- src/sparta_calendar_lambda/core.cljs
+++ src/sparta_calendar_lambda/core.cljs
@@ -1,7 +1,19 @@
 (ns sparta-calendar-lambda.core
-  (:require [cljs-lambda.macros :refer-macros [defgateway]]))
- 
-(defgateway matches
-  [& args]
-  {:status 200
-   :body "{\"hello\": \"world\"}"})
+  (:require [cljs-lambda.macros :refer-macros [defgateway]]
+            [cljs-lambda.util :refer [async-lambda-fn]]
+            [cljs.nodejs :as nodejs]
+            [cljs.core.async :refer [<!]]
+            [eulalie.platform :as eul])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
+
+(defn get [url]
+   (go (let [{:keys [error body status] :as response} (<! (eul/http-get! url))]
+     body)))
+
+(def ^:export matches
+  (async-lambda-fn
+   (fn [event ctx]
+     (go 
+       (let [body (<! (get "http://example.com/"))]
+      {:body body
+      :status 200})))))
