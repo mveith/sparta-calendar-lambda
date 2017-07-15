b85f06717049858d1417ef75497b28b848d96321
mveith <mveith@hotmail.cz>
2017-07-15 15:07:23 +0200

    Parse matches

--- src/sparta_calendar_lambda/core.cljs
+++ src/sparta_calendar_lambda/core.cljs
@@ -3,17 +3,82 @@
             [cljs-lambda.util :refer [async-lambda-fn]]
             [cljs.nodejs :as nodejs]
             [cljs.core.async :refer [<!]]
-            [eulalie.platform :as eul])
+            [eulalie.platform :as eul]
+            [clojure.string :as str]
+            [cljs-time.core :as time])
   (:require-macros [cljs.core.async.macros :refer [go]]))
+
+(def matches-url "http://www.sparta.cz/srv/www/cs/football/match/viewNextMatches.do")
 
 (defn get [url]
    (go (let [{:keys [error body status] :as response} (<! (eul/http-get! url))]
      body)))
 
+(defn get-matches-table [html]
+  (->
+    (str/split html #"<div class=\"tableContainer\">")
+    (second)
+    (str/split #"<table>")
+    (str/split #"</table>")
+    (first)))
+
+(defn get-matches-rows [table]
+  (drop 2 (str/split table #"<tr>")))
+
+(defn get-match-cells [row]
+  (str/split row #"\\n"))
+
+(defn get-td-content [td]
+  (->
+    (str/split td #"<td>")
+    (second)
+    (str/split #"</td>")
+    (first)
+    (str/split #">")
+    (last)
+    (str/split #"<")
+    (first)))
+
+(defn parse-date [date-time-string]
+  (let [parts (filter (fn [s] (not (str/blank? s))) (str/split date-time-string #"[. |:]"))]
+    (let [date-time (time/date-time (int (nth parts 2)) (int (nth parts 1)) (int (nth parts 0)) (int (nth parts 3)) (int (nth parts 4)))]
+      {
+       :day         (time/day date-time)
+       :month       (time/month date-time)
+       :year        (time/year date-time)
+       :hour        (time/hour date-time)
+       :minute      (time/minute date-time)
+       :day-of-week (time/day-of-week date-time)
+       })))
+
+(defn starts-with? [string pattern]
+  (and
+    (>= (count string) (count pattern))
+    (= (subs string 0 (count pattern)) pattern)))
+
+(defn home-match? [home-team-name]
+  (starts-with? home-team-name "AC Sparta Praha"))
+
+(defn create-match [row]
+  (let [cells (get-match-cells row)]
+    {
+     :team          (get-td-content (nth cells 1))
+     :event         (get-td-content (nth cells 3))
+     :home-team     (get-td-content (nth cells 5))
+     :away-team     (get-td-content (nth cells 9))
+     :date          (parse-date (get-td-content (nth cells 10)))
+     :is-home-match (home-match? (get-td-content (nth cells 5)))
+     }))
+
 (def ^:export matches
   (async-lambda-fn
    (fn [event ctx]
      (go 
-       (let [body (<! (get "http://example.com/"))]
-      {:body body
+       (let [html (<! (get matches-url))]
+      {:body (str 
+              (map create-match
+                (->
+                  (get-matches-table html)
+                  (get-matches-rows)
+                  )))
       :status 200})))))
